afe6b21c8eb48703b58bb95688fc5a479b7abc55
M K <mkminek11@seznam.cz>
2023-02-12 19:21:57 +0100

    UPDATE IN browse, search, set

--- search.php
+++ search.php
@@ -4,15 +4,20 @@
     </head>
     <body>
         <?php
-        $name = $_GET["q"];
-
         $conn = mysqli_connect("sql6.webzdarma.cz", "acelingwzcz6315", "Password 1", "acelingwzcz6315");
-        $sets = mysqli_query($conn, "SELECT * FROM `sets` WHERE name LIKE '%$name%'");
-        // foreach ($sets as $set) {
-            // echo $set;
-        // }
+
+        if (!isset($_GET["q"])) {
+            echo '
+                <form action="search.php" autocomplete="off"><input type="text" id="q" name="q"><button type="submit">OK</button></form>
+                <h1>Or browse:</h1>';
+            $sets = mysqli_query($conn, "SELECT * FROM `sets`");
+        } else {
+            $name = $_GET["q"];
+            $sets = mysqli_query($conn, "SELECT * FROM `sets` WHERE name LIKE '%$name%'");
+        }
+
+        if (mysqli_num_rows($sets) == 0) {echo "<h1>No results.</h1>Sorry. We can't find anything like '$name'";}
         while($set = mysqli_fetch_assoc($sets)) {
-            // echo "Id: " . $set["id"]. " - Name: " . $set["name"]. " - Language: " . $set["lang"]. "<br>";
             $name = $set["name"];
             $id = $set["id"];
             echo '<a href="set.php?i='.$id.'"><div style="width: 100px; height: 100px; border: 2px solid black; border-radius: 5px; margin: 10px;">'.$name.'</div></a>';
--- set.php
+++ set.php
@@ -2,51 +2,35 @@
     <head>
         <meta charset="utf-8">
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@300;400;600&display=swap');
-            p {
-                font-family: Montserrat Alternates;
-                text-align: center;
-            }
+            span, h1 {width: 100%; display: inline-block; text-align: center;}
+            td {max-width: 100px; min-width: 100px; overflow: hidden; text-overflow: ellipsis;}
+            table {margin: auto;}
         </style>
-        <script>
-            function turn(id) {
-                div = document.getElementById(id);
-                actual = div.getElementsByTagName("input")[0].value;
-                if (actual == "phrase") {
-                    div.getElementsByTagName("p")[0].style.display = "none";
-                    div.getElementsByTagName("p")[1].style.display = "block";
-                    div.getElementsByTagName("input")[0].value = "translation";
-                } else {
-                    div.getElementsByTagName("p")[0].style.display = "block";
-                    div.getElementsByTagName("p")[1].style.display = "none";
-                    div.getElementsByTagName("input")[0].value = "phrase";
-                }
-            }
-        </script>
     </head>
     <body>
         <?php
         include "insert.inc";
         insert_list($_SESSION);
-
+        
         $id = $_GET["i"];
 
+        echo "<ul><li><a href='cards.php?i=$id'>Karty</a></li></ul>";
+        
         $conn = mysqli_connect("sql6.webzdarma.cz", "acelingwzcz6315", "Password 1", "acelingwzcz6315");
         $words = mysqli_query($conn, "SELECT * FROM `$id`");
         $set = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `sets` WHERE id='$id'"));
-        
-        // echo "Id: " . $set["id"]. " - Name: " . $set["name"]. " - Language: " . $set["lang"]. "<br>";
 
+        echo "<h1>".$set["name"]."</h1>";
+        ?>
+
+        <table>
+        
+        <?php
         $x = 0;
         while ($word = mysqli_fetch_assoc($words)) {
             $phrase = $word["phrase"];
             $translation = $word["translation"];
-            echo '
-            <div style="width: 100px; height: 100px; border: 2px solid black; border-radius: 5px; margin: 10px; user-select: none;" id="'.$x.'" onclick="turn('.$x.')">
-                <p>'.$phrase.'</p>
-                <p style="display: none;">'.$translation.'</p>
-                <input type="hidden" value="phrase">
-            </div>';
+            echo "<tr><td><span>$phrase</span></td><td><span>$translation</span></td></tr>";
             $x += 1;
         }
 
@@ -56,5 +40,6 @@
         }
 
         ?>
+        </table>
     </body>
 </html>
