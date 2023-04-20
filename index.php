2f963b47b36c137b5f8bfbfd503c1003101d48bd
M K <mkminek11@seznam.cz>
2023-04-20 15:39:36 +0200

    GRAPHICS: index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <html>
     <head>
         <title>Aceling - Learn languages smarter!</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" /> <!-- Globe icon -->
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
-        <div>
-            Welcome to Aceling
-            <button onclick='window.location.assign("sign_up.php")'>Get started FOR FREE!</button>
-            <button onclick='window.location.assign("log_in.php")'>Already have an account</button>
+        <div class="earth-bg"></div>
+        <span class="material-symbols-rounded language-icon">language</span>
+        <div class="content">
+            <h2>Welcome to Aceling</h2>
+            <button onclick='window.location.assign("account/auth/sign_up.php")'>Get started <span style="font-weight: bold;">for free</span></button>
+            <button onclick='window.location.assign("account/auth/log_in.php")'>Already have an account</button>
         </div>
     </body>
 </html>
